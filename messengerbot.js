ebeeb0068c32e573513f8883d4c95ef0b2133d26
Albert Wang <ajw@berkeley.edu>
2020-04-09 03:49:39 -0400

    initial commit

---
+++ messengerbot.js
@@ -0,0 +1,63 @@
+const fs = require("fs");
+const login = require("facebook-chat-api");
+const readline = require("readline");
+
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+var login_email = "";
+var login_password = "";
+var msg = "";
+var recipients = [];
+
+console.log("Welcome to the ANova mass DMer. Before you start, have a comma-separated list of your intended recipients, and \n" + 
+    "the message that you want to send stored somewhere else.");
+console.log("This is because the bot will make you restart the process if you enter your login credentials incorrectly oops\n");
+
+rl.question("Enter your Facebook login email: ", function(email) {
+    login_email = email;
+    rl.question("Enter your Facebook password: ", function(pw) {
+        login_password = pw;
+        rl.question("Enter a list of your recipients. Use their full name as it appears on Facebook, and separate names with commas: ", function(recipientsString) {
+            recipients = recipientsString.split(",");
+            rl.question("Enter the message you want to send! ", function(messageToSend) {
+                msg = messageToSend;
+                rl.close();
+            });
+        });
+    });
+});
+
+
+rl.on("close", function() {
+    if (login_email && login_password) {
+        login({email: login_email, password: login_password}, (err, api) => {
+            if (err) {
+                switch (err.error) {
+                    case 'login-approval':
+                        console.log('Enter 2FA code > ');
+                            rl.on('line', (line) => {
+                                err.continue(line);
+                                rl.close();
+                            });
+                            break;
+                    default:
+                        console.error(err);
+                }
+                return;
+            }
+
+            recipients.map(name =>
+                api.getUserID(name, (err, data) => {
+                    if(err) return console.error(err);
+
+                    // Send the message to the best match (best by Facebook's criteria)
+                    var threadID = data[0].userID;
+                    api.sendMessage(msg, threadID);
+                })
+            );
+        });
+    }
+});
